ee8f72cbd213ea8e207da0485a169c3b7d5a8007
JanTheThird <20122613@s.ubaguio.edu>
2026-03-22 21:14:11 +0800

    pages

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import StudentList from "../components/StudentList.tsx";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <StudentList />
+    </div>
+  );
+}
+
+export default Home;
